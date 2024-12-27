0d5c2644213ba7e324f7197a1c97d20a4b450399
Aditya Pant <pantanupam05@gmail.com>
2024-12-27 21:41:13 +0530

    middleware complex

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+function isSmallEnjoughMiddleware(req, res, next) {
+	const age = req.query.age;
+	if (age >= 5) {
+		next();
+	} else {
+		res.json({
+			msg: "Sorry  are not of age yet",
+		});
+	}
+}
 function isOldEnjoughMiddleware(req, res, next) {
 	const age = req.query.age;
 	if (age >= 14) {
@@ -11,12 +21,15 @@ function isOldEnjoughMiddleware(req, res, next) {
 	}
 }
 
-app.get("/ride1", isOldEnjoughMiddleware, function (req, res) {
+app.use(isSmallEnjoughMiddleware);
+app.get("/ride2", function (req, res) {
 	res.json({
 		msg: "you have successfully riden the ride ",
 	});
 });
-app.get("/ride2", isOldEnjoughMiddleware, function (req, res) {
+app.use(isOldEnjoughMiddleware);
+
+app.get("/ride1", function (req, res) {
 	res.json({
 		msg: "you have successfully riden the ride ",
 	});
